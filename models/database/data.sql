cfbda918360c242d3588deca4744f273ebfece40
Cristobal Gonzalez Ibanez <cris.lasiba@gmail.com>
2022-05-02 22:47:25 -0400

    Prototipo v1.0.0

--- models/database/data.sql
+++ models/database/data.sql
@@ -1,5 +1,1 @@
-CREATE DATABASE skatepark;
-CREATE TABLE skaters (id SERIAL, email VARCHAR(50) NOT NULL, nombre
-VARCHAR(25) NOT NULL, password VARCHAR(25) NOT NULL, anos_experiencia
-INT NOT NULL, especialidad VARCHAR(50) NOT NULL, foto VARCHAR(255) NOT
-NULL, estado BOOLEAN NOT NULL);
+CREATE DATABASE monet;
